43900a2f2eb21637fe6788fa089eb3bd7ee65a2c
interactivewebs <david@interactivewebs.com.au>
2025-08-24 16:31:46 +1000

    Fix: use promise pool export in recalls route (pool.execute)

--- backend/routes/recalls.js
+++ backend/routes/recalls.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { verifyToken } = require('../middleware/auth');
-const pool = require('../config/database');
+const { pool } = require('../config/database');
 
 const router = express.Router();
 
